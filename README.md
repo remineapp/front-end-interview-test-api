64c543d8f66abcd3054384f102a13b50082b2aa2
bdell <brandon@remine.com>
2017-10-16 11:34:43 -0400

    adds users and subscriptions

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### Front End Interview Test API
 
+#### Usage
 This fake server uses `faker.js` and `json-server` in order to serve up fake location data.
 
 Run default server with `npm start`, this will serve up the contents in `db.json`.
@@ -10,7 +11,9 @@ We will be checking the final submission against the `final-test.json` file.
 
 You may launch the server with the `final-test.json` file by running `npm run check`.
 
-The API route that serves the data is `http://localhost:8001/locations`, and the data it serves up is of shape:
+#### Data Shapes
+
+The API route that serves the location data is `http://localhost:8001/locations`, and the data it serves up is of shape:
 ```
 {
     "id": 0,
@@ -21,4 +24,75 @@ The API route that serves the data is `http://localhost:8001/locations`, and the
 }
 ```
 
-Additionally, the API route that serves the building types is `http://localhost:8001/buildingTypes`.
+The API route that serves the building types is `http://localhost:8001/buildingTypes`.
+
+The API route for all subscriptionPlans is `http://localhost:8001/subscriptionPlans`.  The data shape is as follows:
+```
+[
+  {
+    "id": 1,
+    "maxTrackedItems": 100,
+    "creditsPerHousehold": 3,
+    "price": {
+      "currency": "",
+      "value": "Free",
+      "paymentInterval": ""
+    },
+    "additionalFeatures": [
+      "Sell Scores"
+    ]
+  },
+  {
+    "id": 2,
+    "maxTrackedItems": 250,
+    "creditsPerHousehold": 2.5,
+    "price": {
+      "currency": "USD",
+      "value": "29",
+      "paymentInterval": "month"
+    },
+    "additionalFeatures": [
+      "Sell Scores",
+      "Value & Equity Maps"
+    ]
+  },
+  {
+    "id": 3,
+    "maxTrackedItems": 500,
+    "creditsPerHousehold": 1.5,
+    "price": {
+      "currency": "USD",
+      "value": "99",
+      "paymentInterval": "month"
+    },
+    "additionalFeatures": [
+      "Sell Scores",
+      "Value & Equity Maps"+
+    ]
+  },
+  {
+    "id": 4,
+    "maxTrackedItems": 10000,
+    "creditsPerHousehold": 1,
+    "price": {
+      "currency": "USD",
+      "value": "199",
+      "paymentInterval": "month"
+    },
+    "additionalFeatures": [
+      "Sell Scores",
+      "Value & Equity Maps"
+    ]
+  }
+]
+```
+
+The API route for all users is `http://localhost:8001/user`.  You can request a single user object by appending the id to the previous URL like so: `http://localhost:8001/user/1`.  The data shape for a user is:
+```
+{
+  "id": 1,
+  "userPlan": {
+    "planId": 1
+  }
+}
+```
--- generateDB.js
+++ generateDB.js
@@ -14,21 +14,51 @@ const generateStreetAddress = () => {
     const zipCode = faker.address.zipCode();
     const state = faker.address.state();
     return `${streetAddress} ${city} ${state}, ${zipCode}`;
-}
+};
 
-const buildingTypes = [
-    "multiFamily",
-    "condo",
-    "business",
-    "office",
-    "singleFamily"
-];
+const buildingTypes = ['multiFamily', 'condo', 'business', 'office', 'singleFamily'];
 
 const jsonFile = 'db.json';
 
 let jsonOutput = {
     locations: [],
-    buildingTypes
+    user: [
+        { id: 1, userPlan: { planId: 1 } },
+        { id: 2, userPlan: { planId: 4 } },
+        { id: 3, userPlan: { planId: 2 } },
+        { id: 4, userPlan: { planId: 3 } },
+    ],
+    subscriptionPlans: [
+        {
+            id: 1,
+            maxTrackedItems: 100,
+            creditsPerHousehold: 3,
+            price: { currency: '', value: 'Free', paymentInterval: '' },
+            additionalFeatures: ['Sell Scores'],
+        },
+        {
+            id: 2,
+            maxTrackedItems: 250,
+            creditsPerHousehold: 2.5,
+            price: { currency: 'USD', value: '29', paymentInterval: 'month' },
+            additionalFeatures: ['Sell Scores', 'Value & Equity Maps'],
+        },
+        {
+            id: 3,
+            maxTrackedItems: 500,
+            creditsPerHousehold: 1.5,
+            price: { currency: 'USD', value: '99', paymentInterval: 'month' },
+            additionalFeatures: ['Sell Scores', 'Value & Equity Maps'],
+        },
+        {
+            id: 4,
+            maxTrackedItems: 10000,
+            creditsPerHousehold: 1,
+            price: { currency: 'USD', value: '199', paymentInterval: 'month' },
+            additionalFeatures: ['Sell Scores', 'Value & Equity Maps'],
+        },
+    ],
+    buildingTypes,
 };
 
 for (var i = 0; i < numberOfEntries; i++) {
@@ -38,7 +68,7 @@ for (var i = 0; i < numberOfEntries; i++) {
     if (i === 25 || i === 20 || i === 35) {
         numberOfBeds = 6;
     } else if (i === 68) {
-        numberOfBeds = "4";
+        numberOfBeds = '4';
     } else if (i === 75) {
         numberOfBeds = 98;
     } else {
@@ -54,11 +84,11 @@ for (var i = 0; i < numberOfEntries; i++) {
     }
 
     const generatedJsonObject = {
-        "id": i,
-        "address": generateStreetAddress(),
-        "beds": numberOfBeds,
-        "baths": numberOfBaths,
-        "buildingType": faker.random.arrayElement(buildingTypes)
+        id: i,
+        address: generateStreetAddress(),
+        beds: numberOfBeds,
+        baths: numberOfBaths,
+        buildingType: faker.random.arrayElement(buildingTypes),
     };
 
     jsonOutput.locations.push(generatedJsonObject);
